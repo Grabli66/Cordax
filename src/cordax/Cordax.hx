a39923e3e47fc72f14ec4e4a8e49ea112b37e146
Grabli66 <grabli66@gmail.com>
2018-11-26 16:44:29 +0300

    Start from scratch

--- src/cordax/Cordax.hx
+++ src/cordax/Cordax.hx
@@ -3,28 +3,27 @@ package cordax;
 #if js
 import js.Browser;
 #end
-
 import cordax.ui.View;
 
 /**
  * Helper
  */
 class Cordax {
-    /**
-     * Run application with root view
-     * @param view 
-     */
-    public static function run(view:View){
-        #if js
-            Browser.document.appendChild(view.toElement().toNative());
-        #end
-    }
+	/**
+	 * Run application with root view
+	 * @param view
+	 */
+	public static function run(view:View) {
+		#if js
+		Browser.document.appendChild(view.toElement().toNative());
+		#end
+	}
 
-    /**
-     * Set title to application
-     * @param title 
-     */
-    public static function setTitle(title:String) {
-        Browser.document.getElementsByTagName('title')[0].innerText = title;
-    }
+	/**
+	 * Set title to application
+	 * @param title
+	 */
+	public static function setTitle(title:String) {
+		Browser.document.getElementsByTagName('title')[0].innerText = title;
+	}
 }
