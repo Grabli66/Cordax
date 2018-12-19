78d4f2862aa4926183c44f28a24065b1d798c88a
Grabli66 <grabli66@gmail.com>
2018-12-19 22:13:46 +0300

    ListView

--- src/cordax/ui/ListView.hx
+++ src/cordax/ui/ListView.hx
@@ -4,13 +4,27 @@ import cordax.native.elements.Element;
 import cordax.native.elements.RootElement;
 
 /**
+ * Adapter for list view items
+ */
+class ListViewAdapter {
+    /**
+     * Render item to view
+     * @param data 
+     * @return View
+     */
+    public function render(data:Dynamic):View {
+        throw "Not implemented";
+    }
+}
+
+/**
  * List with items
  */
 class ListView extends View {
     /**
      * Constructor
      */
-    public function new() {
+    public function new(adapter:ListViewAdapter) {
         super();
     }
 
